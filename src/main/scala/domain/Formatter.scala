0185d1524ed963dd95ab76f48e90ff6790897c50
mtucci <matteo.detucci_ext@scout24.com>
2019-03-12 21:56:08 +0100

    Extract separator in its own private method

--- src/main/scala/domain/Formatter.scala
+++ src/main/scala/domain/Formatter.scala
@@ -6,10 +6,14 @@ case class Formatter() {
   private val tennisPoints = Map(0 -> "0", 1 -> "15", 2 -> "30", 3 -> "40")
 
   def formatMatchScore(set: Set, player1: Player, player2: Player): String = {
-    gameScore(set, player1, player2) + currentGameScore(set, player1, player2)
+    gamesScore(set, player1, player2) + separator(set) + currentGameScore(set, player1, player2)
   }
 
-  private def gameScore(set: Set, player1: Player, player2: Player) = {
+  private def separator(set: Set): String = {
+    set.currentGame().fold("")(_ => ", ")
+  }
+
+  private def gamesScore(set: Set, player1: Player, player2: Player) = {
     s"${set.gamesWonByPlayer(player1)}-${set.gamesWonByPlayer(player2)}"
   }
 
@@ -22,19 +26,19 @@ case class Formatter() {
 
   private def normalGame(normalGame: NormalGame, player1: Player, player2: Player): String = {
     if (normalGame.isDeuce) {
-      return ", Deuce"
+      return "Deuce"
     }
 
     if (normalGame.isAdvantage) {
-      return s", Advantage ${normalGame.leadingPlayerName()}"
+      return s"Advantage ${normalGame.leadingPlayerName()}"
     }
 
-    s", ${toTennisPoints(normalGame.pointsForPlayer(player1))}-${toTennisPoints(normalGame.pointsForPlayer(player2))}"
+    s"${toTennisPoints(normalGame.pointsForPlayer(player1))}-${toTennisPoints(normalGame.pointsForPlayer(player2))}"
   }
 
   private def toTennisPoints(points: Int): String =
     tennisPoints.get(points).fold("")(Predef.identity)
 
   private def tieBreakGame(game: TieBreak, player1: Player, player2: Player): String =
-    s", ${game.pointsForPlayer(player1)}-${game.pointsForPlayer(player2)}"
+    s"${game.pointsForPlayer(player1)}-${game.pointsForPlayer(player2)}"
 }
