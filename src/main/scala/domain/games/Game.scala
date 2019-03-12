410a740acec4944263b6e39d5ec3f18cd6c305e4
mtucci <matteo.detucci_ext@scout24.com>
2019-03-12 21:42:54 +0100

    Make Game a sealed abstract class

--- src/main/scala/domain/games/Game.scala
+++ src/main/scala/domain/games/Game.scala
@@ -2,7 +2,7 @@ package domain.games
 
 import domain.{Player, Score}
 
-abstract class Game(private val player1: Player, private val player2: Player) {
+sealed abstract class Game(private val player1: Player, private val player2: Player) {
   protected var score = Score(player1, player2, 0, 0)
 
   def wonBy(): Option[Player]
@@ -18,4 +18,51 @@ abstract class Game(private val player1: Player, private val player2: Player) {
   def leadingPlayerName(): String =
     score.leader().name
 
+}
+
+class NormalGame(private val player1: Player, private val player2: Player) extends Game(player1, player2) {
+
+  def wonBy(): Option[Player] = {
+    if (isDeuce) {
+      return None
+    }
+
+    if (isAdvantage) {
+      return None
+    }
+
+    if (score.hasPlayerAtLeastPoints(player1, 4)) {
+      return Some(player1)
+    }
+
+    if (score.hasPlayerAtLeastPoints(player2, 4)) {
+      return Some(player2)
+    }
+
+    None
+  }
+
+  def isDeuce: Boolean =
+    haveBothPlayersAtLeastPoints(3) && score.isTie
+
+  def isAdvantage: Boolean =
+    haveBothPlayersAtLeastPoints(3) && score.isPointsDifference(1)
+
+  private def haveBothPlayersAtLeastPoints(points: Int) =
+    score.hasPlayerAtLeastPoints(player1, points) && score.hasPlayerAtLeastPoints(player2, points)
+}
+
+class TieBreak(private val player1: Player, player2: Player) extends Game(player1, player2) {
+
+  def wonBy(): Option[Player] = {
+    if (score.hasPlayerAtLeastPoints(player1, 7) && score.isPointsDifference(2)) {
+      return Some(player1)
+    }
+
+    if (score.hasPlayerAtLeastPoints(player1, 7) && score.isPointsDifference(2)) {
+      return Some(player2)
+    }
+
+    None
+  }
 }
